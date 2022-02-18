4a54fc8f31e799671342016d5851feb8d4ba759e
AryaaSk <aryaa_saravana@outlook.com>
2022-02-18 13:42:53 +0400

    fixed the drag and drop mechanism

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,30 +13,23 @@ export class AppComponent {
   title = 'pointGame';
 
   correctPoints = ["blue", "red", "green", "purple", "orange"]
-  userPoints = this.shuffle(["blue", "red", "green", "purple", "orange"])
 
+  userPoints = this.shuffle(["blue", "red", "green", "purple", "orange"])
   droppedUser(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.userPoints, event.previousIndex, event.currentIndex);
+    moveItemInArray( this.userPoints, event.previousIndex, event.currentIndex );
   }
 
   shuffle(array: string[]) {
     let currentIndex = array.length,  randomIndex;
-  
-    // While there remain elements to shuffle...
     while (currentIndex != 0) {
-  
-      // Pick a remaining element...
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex--;
-  
-      // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
+      [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
     }
     return array;
   }
 
-
+  //testing code:
   movies = [
     'Episode I - The Phantom Menace',
     'Episode II - Attack of the Clones',
@@ -49,9 +42,12 @@ export class AppComponent {
     'Episode IX - The Rise of Skywalker',
   ];
 
-  drop(event: any) {
-    moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
+  dropped(event: CdkDragDrop<string[]>) {
+    moveItemInArray(
+       this.movies,
+       event.previousIndex,
+       event.currentIndex
+      );
   }
-
 
 }
