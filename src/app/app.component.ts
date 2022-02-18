ab59de2bceb88b4b5f022e626386178bb7a4ad59
AryaaSk <aryaa_saravana@outlook.com>
2022-02-18 08:30:31 +0400

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor (){}
+
   title = 'pointGame';
+
+  correctPoints = ["blue", "red", "green", "purple", "orange"]
+  userPoints = this.shuffle(["blue", "red", "green", "purple", "orange"])
+
+
+
+
+  shuffle(array: string[]) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+    return array;
+  }
 }
